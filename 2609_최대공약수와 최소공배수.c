62435ed6876340beeb9d3c2eff841805e2b619eb
JS <zhujack26@gmail.com>
2023-09-24 15:53:51 +0900

    baekjoon_2609

---
+++ 2609_최대공약수와 최소공배수.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a, b, i, result_max, result_min;
+    result_min = 1;
+    i = 1;
+    
+    scanf("%d %d", &a, &b);
+    while(i <= a || i <= b)
+    {
+        if ((a % i == 0) && (b % i == 0))
+            result_max = i; 
+        i++;
+    }
+    i = 2;
+    while(i <= a || i <= b)
+    {
+        if ((a % i == 0) && (b % i == 0))
+        {
+            a = a / i;
+            b = b / i;
+            result_min = result_min * i;
+        }
+        else if((a % i != 0) || (b % i != 0))
+            i++;
+        //최대공약수 구하는 법 (공통 소인수로 몫이 서로소가 될 때까지 나오고 모두 곱하기)
+    }
+    result_min = result_min * a * b;
+    printf("%d\n%d", result_max, result_min);
+    return 0;
+}
