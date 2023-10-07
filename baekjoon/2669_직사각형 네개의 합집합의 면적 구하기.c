283593c7aa3ae44192db3e3b9642d812483fdc89
JS <zhujack26@gmail.com>
2023-10-08 00:19:23 +0900

    fix: 2669

--- baekjoon/2669_직사각형 네개의 합집합의 면적 구하기.c
+++ baekjoon/2669_직사각형 네개의 합집합의 면적 구하기.c
@@ -2,30 +2,44 @@
 
 int main() 
 {
-    int array[101][101];
-    int x, y, x_1, y_1;
     int i = 0;
-    int count = 0;
-    while (i < 4)
+    int array[101][101];
+    while (i < 101)
     {
-        scanf("%d, %d, %d, %d", &x, &y, &x_1, &y_1);
-        while(x <= x_1)
+        int j = 0;
+        while (j < 101)
         {
-            array[x][y] = 1;
-            y++;
-            if (y == y_1)
+            array[i][j] = 0;
+            j++;
+        }
+        i++;
+    }
+    
+    int x, y, x_1, y_1;
+    int count = 0;
+    
+    int index = 1;
+    while (index <= 4)
+    {
+        scanf("%d %d %d %d", &x, &y, &x_1, &y_1);
+        int j = 0;
+        while(x <= x_1-1)
+        {
+            array[x][y+j] = 1;
+            j++;
+            if (y+j == y_1)
             {
                 x++;
                 j = 0;
             }
         }
-        i++;
+        index++;
     }
     i = 1;
-    while (i < 101)
+    while (i <= 100)
     {
         int j = 1;
-        while (j< 101)
+        while (j<= 100)
         {
             if (array[i][j] == 1)
                 count++;
