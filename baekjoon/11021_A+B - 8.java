4e73e76d148477c603e8bd4bd444217016934213
JS <zhujack26@gmail.com>
2023-11-02 01:29:26 +0900

    add : 11021

---
+++ baekjoon/11021_A+B - 8.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int result = A + B;
+            bw.write("Case #" + i + ": "+ A + " + " + B + " = " + result);
+            bw.write("\n");
+        }
+        bw.close();
+    }
+}
