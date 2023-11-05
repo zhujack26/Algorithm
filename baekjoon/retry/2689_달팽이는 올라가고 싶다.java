d4b724fe8b23bbc54777295fb785eb9f4914bd82
JS <zhujack26@gmail.com>
2023-11-05 19:58:49 +0900

    baekjoon : 2869_math

---
+++ baekjoon/retry/2689_달팽이는 올라가고 싶다.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+        int count = (V - B) / (A - B);
+        if ((V - B) % (A - B) != 0)
+            count++;
+        bw.write("" + count);
+        bw.close();
+    }
+}
