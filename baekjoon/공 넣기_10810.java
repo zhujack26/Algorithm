75454789bc9a0c69b9afaf2c731aa281ff58eabf
JS <zhujack26@gmail.com>
2023-11-16 00:09:30 +0900

    baekjoon : 10810

---
+++ baekjoon/공 넣기_10810.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        for (int h = 0; h < M; h++) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+            for (int l = i; l <= j; l++)
+                arr[l-1] = k;
+        }
+        for (int l = 0; l < N; l++)  
+                bw.write(arr[l] + " ");
+        bw.close();
+    }
+}
