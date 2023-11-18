1da891060a5bb2a50a7932224656ba95baad1fe7
JS <zhujack26@gmail.com>
2023-11-18 23:58:10 +0900

    baekjoon_27866

---
+++ baekjoon/27866_문자와 문자열.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String S = br.readLine();
+        int i = Integer.parseInt(br.readLine());
+        bw.write(S.charAt(i-1));
+        bw.close();
+    }
+}
