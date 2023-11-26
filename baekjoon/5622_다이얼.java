aa07ef366d6d3a05f3bb736aa1447b4048be67bf
JS <zhujack26@gmail.com>
2023-11-27 00:04:47 +0900

    baekjoon : 5622

---
+++ baekjoon/5622_다이얼.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String input = br.readLine();
+        char[] alphabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+        int[] dial = {3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 10, 10, 10, 10};
+        int result = 0;
+        for (int i = 0; i < input.length(); i++) {
+            for (int j = 0; j < 26; j++) {   
+                if (input.charAt(i) == alphabet[j])
+                    result = result + dial[j];
+            }
+        }
+        bw.write(String.valueOf(result));
+        bw.close();
+    }
+}
