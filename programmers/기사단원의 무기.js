6ea016ccb1c1a27df0caa2ff6b7fd2b2e61d4b9c
Jack Park <112409764+zhujack26@users.noreply.github.com>
2024-01-21 03:03:34 +0900

    Update 기사단원의 무기.js

--- programmers/기사단원의 무기.js
+++ programmers/기사단원의 무기.js
@@ -18,7 +18,11 @@ function solution(number, limit, power) {
     }
     return result;
 }
-/* 
-두번째 for문의 최대범위를 i의 제곱근으로 두어 시간 복잡도 줄이기
+/*
+1. JavaScript에서 배열은 let i = [];과 같이 생성할 수 있으나 
+세트는 객체가 아닌 다른 데이터 구조이므로 let i = {};과 같이 객체 리터럴 문법으로는 생성할 수 없다.
+let i = new Set();
+
+2. 두번째 for문의 최대범위를 i의 제곱근으로 두어 시간 복잡도 줄이기
 https://kbw1101.tistory.com/32
 */
