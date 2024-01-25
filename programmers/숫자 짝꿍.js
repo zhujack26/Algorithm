07740d13153d418dd6d30fc10d466f34dcd10139
Jack Park <112409764+zhujack26@users.noreply.github.com>
2024-01-26 00:56:13 +0900

    Update 숫자 짝꿍.js

--- programmers/숫자 짝꿍.js
+++ programmers/숫자 짝꿍.js
@@ -19,3 +19,24 @@ function solution(X, Y) {
 /* 테케 11~15 시간 초과
 해시맵 혹은 다른 방법 강구
 */
+
+
+// 각 포문에 3000000개씩 들어가므로 시간초과 -> while문 하나로 수정
+
+function solution(X, Y) {
+    X = X.split('').sort().reverse();
+    Y = Y.split('').sort().reverse();
+    let i = 0, j = 0;
+    let answer = "";
+    while (i < X.length && j < Y.length) {
+        if (X[i] == Y[j]) {
+            answer += (X[i]); 
+            i++; j++;
+        }
+        else if (X[i] > Y[j])
+            i++;
+        else if (X[i] < Y[j])
+            j++;
+    }
+    return answer.length == 0 ? "-1" : (answer[0] == 0 ? "0" : answer);
+}
