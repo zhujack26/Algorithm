0dfb8d012a15e8e401b1845bf7a6f4d456d0e822
Jack Park <112409764+zhujack26@users.noreply.github.com>
2024-01-29 16:55:38 +0900

    Update 로또의 최고 순위와 최저 순위.js

--- programmers/로또의 최고 순위와 최저 순위.js
+++ programmers/로또의 최고 순위와 최저 순위.js
@@ -1,3 +1,4 @@
+/*
 function solution(lottos, win_nums) {
     lottos.sort();
     win_nums.sort();
@@ -21,4 +22,22 @@ function solution(lottos, win_nums) {
     let lotto = {0 : 6, 1 : 6, 2 : 5, 3 : 4, 4 : 3, 5 : 2, 6 : 1}
     return [lotto[count+possible], lotto[count]]; 
 }
-// 미완성 15/9개 정답
+
+1) 숫자에 sort()써서 문제가 있었음  .sort((a,b) => a-b); 사용하여 해결.
+2) 아래와 같이 수정하여 시간복잡도 성능 향상.
+*/
+
+function solution(lottos, win_nums) {
+    let count = 0;
+    let possible = 0;
+    for (let i of lottos) {
+        if (i == 0) {
+            possible++;
+        } 
+        else if (win_nums.includes(i)) {
+            count++;
+        }
+    }
+    let lotto = {0 : 6, 1 : 6, 2 : 5, 3 : 4, 4 : 3, 5 : 2, 6 : 1}
+    return [lotto[count+possible], lotto[count]]; 
+}
