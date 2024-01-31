773ad0db780a99e38bf191143f1bcf7689a5ca22
Jack Park <112409764+zhujack26@users.noreply.github.com>
2024-01-31 13:08:27 +0900

    Update 달리기 경주.js

--- programmers/달리기 경주.js
+++ programmers/달리기 경주.js
@@ -1,12 +1,25 @@
-//저도 시간초과.. 방법 생각 중 -> 해시맵
+/* 시간초과.. 방법 생각 중 -> 해시맵
 function solution(players, callings) {
     for(let i of callings) {
         let num = players.indexOf(i);
-        /*
-        players[num] = players[num-1];
-        players[num-1] = i;
-        */
         [players[num-1], players[num]] = [players[num], players[num-1]];
+    }
+    return players;
+}
+*/
+
+//정답 코드
+function solution(players, callings) {
+    const playerMap = {};
+    players.forEach((player, index) => {
+        playerMap[player] = index;
+    });
+    for (let i = 0; i < callings.length; i++) {
+        let calling = callings[i];
+        let num = playerMap[calling];
+        [players[num-1], players[num]] = [players[num], players[num-1]];
+        playerMap[players[num-1]] = num - 1;
+        playerMap[players[num]] = num;
     }
     return players;
 }
