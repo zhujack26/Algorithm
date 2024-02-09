04ed2bb7e2dccb5dc9a04eba5d33a0d52bbcc537
Jack Park <112409764+zhujack26@users.noreply.github.com>
2024-02-09 17:57:48 +0900

    Update and rename 옹알이.js to 옹알이 (1).js

--- programmers/옹알이 (1).js
+++ programmers/옹알이 (1).js
@@ -12,6 +12,8 @@ function solution(babbling) {
     return result;
 }
 /*
+정규 표현식(Regular Expression, RegExp)을 사용하여 검색하고, 정규 표현식과 일치하는지 여부를 확인.
+일치하면 true, 그렇지 않으면 false를 반환.
 /a-z/.test(babbling[i]) : 이 표현식은 문자열 babbling[i]가 "a-z"라는 문자열을 포함하고 있는지를 확인. "a-z"라는 국어를 그대로 찾는 것.
 /[a-z]/.test(babbling[i]) : 이 표현식은 문자열 babbling[i]에 알파벳 소문자가 하나라도 포함되어 있는지를 확인. 여기서 [a-z]는 알파벳 소문자 전체를 의미하는 범위.
 */
