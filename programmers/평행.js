edde04c5230d919d9877ca1f908da0fa59192d81
Jack Park <112409764+zhujack26@users.noreply.github.com>
2024-02-11 06:03:53 +0900

    Create 평행.js

---
+++ programmers/평행.js
@@ -0,0 +1,11 @@
+function solution(dots) {
+    let slope = [];
+    if ((dots[0][0] - dots[1][0]) / (dots[0][1] - dots[1][1]) == (dots[2][0] - dots[3][0]) / (dots[2][1] - dots[3][1]))
+        return 1;
+    else if ((dots[1][0] - dots[2][0]) / (dots[1][1] - dots[2][1]) == (dots[0][0] - dots[3][0]) / (dots[0][1] - dots[3][1]))
+        return 1;
+    else if ((dots[0][0] - dots[2][0]) / (dots[0][1] - dots[2][1]) == (dots[1][0] - dots[3][0]) / (dots[1][1] - dots[3][1]))
+        return 1;
+    else
+        return 0;
+}
